f38dd442b8cfb383e3910f8f2cd8651e95c99ff8
ecacerest <ecacerest@finagil.com.mx>
2018-04-06 18:29:31 -0500

    Correcion del saoldo insoluto de AP

--- Modulos/mTraeSald.vb
+++ Modulos/mTraeSald.vb
@@ -10,22 +10,28 @@ Module mTraeSald
 
     Public Sub TraeSald(ByVal drVencimientos As DataRow(), ByVal cFeven As String, ByRef nSaldo As Decimal, ByRef nInteres As Decimal, ByRef nCartera As Decimal, TablaEQ As Boolean, ByVal cTipar As String)
         Dim drVencimiento As DataRow
-        'esta parte trae la opcion a compra no pagada del Arrendamiento puro
-        If drVencimientos.Length > 0 Then
-            drVencimiento = drVencimientos(0)
-            If cTipar = "P" Then
-                Dim Ta As New ProductionDataSetTableAdapters.OpcionesTableAdapter
-                If Ta.SacaOpcion(drVencimiento("Anexo")) > 0 And TablaEQ = True Then
-                    nSaldo = Ta.SacaOpcion(drVencimiento("Anexo"))
-                End If
-            End If
-        End If
+        ''esta parte trae la opcion a compra no pagada del Arrendamiento puro
+        'If drVencimientos.Length > 0 Then
+        '    drVencimiento = drVencimientos(0)
+        '    If cTipar = "P" Then
+        '        Dim Ta As New ProductionDataSetTableAdapters.OpcionesTableAdapter
+        '        If Ta.SacaOpcion(drVencimiento("Anexo")) > 0 And TablaEQ = True Then
+        '            nSaldo = Ta.SacaOpcion(drVencimiento("Anexo"))
+        '        End If
+        '    End If
+        'End If
 
         ' Esta variable datarow contendrá los datos de 1 vencimiento a la vez, de la tabla Edoctav, Edoctas o Edoctao
 
         For Each drVencimiento In drVencimientos
             If (drVencimiento("Feven") >= cFeven And drVencimiento("IndRec") = "S") Or drVencimiento("Nufac") = 0 Then
-                nSaldo += drVencimiento("Abcap")
+                If cTipar = "P" Then
+                    If nSaldo = 0 Then
+                        nSaldo = drVencimiento("Saldo") ' toma el saldo insuoluto de la tabla no facturado
+                    End If
+                Else
+                    nSaldo += drVencimiento("Abcap")
+                End If
                 nInteres += drVencimiento("Inter")
                 nCartera += drVencimiento("Abcap") + drVencimiento("Inter")
             End If
@@ -33,7 +39,6 @@ Module mTraeSald
         nSaldo = Round(nSaldo, 2)
         nInteres = Round(nInteres, 2)
         nCartera = Round(nCartera, 2)
-
     End Sub
 
 End Module
