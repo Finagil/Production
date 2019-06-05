8de724b540c9da940784473c39b9ba58f6e8828e
ecacerest <ecacerest@finagil.com.mx>
2019-06-05 12:57:22 -0500

    Correcion tasa iva interes no aplica para Personas morales

--- Mudulo_PROM/frmActiAnexCS.vb
+++ Mudulo_PROM/frmActiAnexCS.vb
@@ -5714,7 +5714,7 @@ Public Class frmActiAnexCS
         End If
 
         Dim taIVAinteres As New ContaDSTableAdapters.CONT_AutorizarIVA_InteresTableAdapter
-        If taIVAinteres.EstaAutorizado(Anexo, "") <= 0 Then
+        If taIVAinteres.EstaAutorizado(Anexo, "") <= 0 And cTippe = "E" Then
             RevisaTasa = True
             MessageBox.Show("Este contrato requiere autorización para NO generar Iva de los intereses.", "Autorización", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Exit Function
