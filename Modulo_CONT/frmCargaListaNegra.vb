6269bc89d8c2467f9e90b76eafebc66db792b7bd
viapolo <viapolo@finagil.com.mx>
2019-07-05 12:11:59 -0500

    - Depura archivos erroneos y se ajusta al nuevo formato del layout

--- Modulo_CONT/frmCargaListaNegra.vb
+++ Modulo_CONT/frmCargaListaNegra.vb
@@ -9,10 +9,11 @@ Public Class frmCargaListaNegra
         openFileDialog1.Filter = "txt files (*.csv)|*.csv"
         openFileDialog1.FilterIndex = 2
         openFileDialog1.RestoreDirectory = True
+        openFileDialog1.Multiselect = True
 
         If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
 
-            Dim objReader As New System.IO.StreamReader(openFileDialog1.FileName)
+
             Dim sLine() As String
             Dim arrText As New ArrayList()
             Dim cont As Integer = 0
@@ -24,43 +25,66 @@ Public Class frmCargaListaNegra
             Dim ta69F As New CreditoDSTableAdapters.CRED_ListaFechaArf69TableAdapter
 
             ta69F.DeleteQuery()
+            ta69.DeleteQuery()
+            ta69B.DeleteQuery()
             ta69F.Insert(Date.Now.ToString)
-            Try
-                While Not objReader.EndOfStream
-                    Try
-                        sLine = objReader.ReadLine().Split(",")
 
-                        If sLine(0) = "RFC" Then
-                            bandera = "Art69"
-                            ta69.DeleteQuery()
-                        ElseIf sLine(0) = "No." Then
-                            ta69B.DeleteQuery()
-                            bandera = "Art69B"
-                        End If
+            For Each files As String In openFileDialog1.FileNames
+                Dim objReader As New System.IO.StreamReader(files)
+                Try
+                    While Not objReader.EndOfStream
+                        Try
+                            Dim lineas As String = objReader.ReadLine() '.Replace(Chr(34), "").Replace(", ", " ").Replace(",, ", " ").Replace(",.", " ")
+                            sLine = lineas.Split(",")
 
-                        If bandera = "Art69" And cont > 0 Then
-                            ta69.Insert(sLine(0), sLine(2), sLine(3), sLine(1))
-                        ElseIf bandera = "Art69B" And cont > 2 Then
-                            ta69B.Insert(sLine(1), sLine(3), sLine(2))
-                        End If
-                        cont += 1
-                        tssbContador.Text = "Leyendo línea: " + cont.ToString + " RFC: " + sLine(1) + " / " + sLine(2)
-                        Me.Update()
-                        sLine = Nothing
-                    Catch ex1 As Exception
-                        'MsgBox(ex1.ToString)
-                        contE += 1
-                    End Try
-                End While
-            Catch ex As Exception
-                'errores += "Error: " + ex.ToString + " en linea " + cont.ToString + vbNewLine
-                MsgBox(cont.ToString,, "Líneas procesadas...")
-                MsgBox(contE.ToString,, "Líneas con errores...")
-            End Try
-            objReader.Close()
-            MsgBox(cont.ToString,, "Líneas procesadas...")
-            MsgBox(contE.ToString,, "Líneas con errores...")
-            MsgBox("Proceso terminado...")
+                            If sLine(0) = "RFC" Then
+                                bandera = "Art69"
+                            ElseIf sLine(0) = "No" Then
+                                bandera = "Art69B"
+                            End If
+
+                            If bandera = "Art69" And cont > 0 Then
+                                sLine = lineas.Split(Chr(34))
+                                Dim sLinea As String
+                                If sLine.Length > 1 Then
+                                    sLinea = sLine(0) & sLine(1).Replace(",", "").Replace(Chr(34), "") & sLine(2)
+                                Else
+                                    sLinea = sLine(0)
+                                End If
+
+                                sLine = sLinea.Split(",")
+                                    ta69.Insert(sLine(0), sLine(2), sLine(3), sLine(1))
+                                ElseIf bandera = "Art69B" And cont > 2 Then
+                                    sLine = lineas.Split(Chr(34))
+                                Dim sLineb As String
+                                If sLine.Length > 1 Then
+                                    sLineb = sLine(0) & sLine(1).Replace(",", "").Replace(Chr(34), "") & sLine(2)
+                                Else
+                                    sLineb = sLine(0)
+                                End If
+
+                                sLine = sLineb.Split(",")
+                                ta69B.Insert(sLine(1), sLine(3), sLine(2))
+                            End If
+                            cont += 1
+                            tssbContador.Text = "Leyendo línea: " + cont.ToString + " RFC: " + sLine(1) + " / " + sLine(2)
+                            Me.Update()
+                            sLine = Nothing
+                        Catch ex1 As Exception
+                            'MsgBox(ex1.ToString)
+                            contE += 1
+                        End Try
+                    End While
+                Catch ex As Exception
+                    'errores += "Error: " + ex.ToString + " en linea " + cont.ToString + vbNewLine
+                    MsgBox(cont.ToString, MsgBoxStyle.Exclamation, "Líneas procesadas...")
+                    MsgBox(contE.ToString, MsgBoxStyle.Exclamation, "Líneas con errores...")
+                End Try
+                objReader.Close()
+            Next
+            MsgBox(cont.ToString, MsgBoxStyle.Exclamation, "Líneas procesadas...")
+            MsgBox(contE.ToString, MsgBoxStyle.Exclamation, "Líneas con errores...")
+            MsgBox("Proceso terminado...", MsgBoxStyle.Information)
         End If
     End Sub
 End Class
